1e6f25e37331b54ac97ad531c142e1ace08125aa
kejihela <gleebayour@gmail.com>
2024-06-30 05:22:23 -0400

    model trainer

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
 
-"""HYPEN_E_DOT='-e .'
+'''HYPEN_E_DOT='-e .'
 
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
@@ -12,7 +12,7 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-    return requirements"""
+    return requirements'''
 
 setup(
     name='PricePredictionForDiamond',
@@ -20,5 +20,6 @@ setup(
     author='Adebayo Abdulganiyu Keji',
     author_email='Gleebayour@gmail.com',
     install_requires=["scikit-learn","pandas","numpy"],
-    packages=find_packages()
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
 )
